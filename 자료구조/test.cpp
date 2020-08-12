db56ea68a13ac94c86e947357b6585c2314f91e3
hhhminme <kenma409@gmail.com>
2020-08-12 14:57:36 +0900

    heapsort

---
+++ 자료구조/test.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int number = 9; 
+int heap[9] = { 7,6,5,8,3,5,9,1,6 };
+
+int main(void) {
+	// 힙 구성 
+	for (int i = 1; i < number; i++) {
+		int c = i; 
+		do {
+			int root = (c - 1) / 2; 
+			if (heap[root] < heap[c]) {
+				int temp = heap[root];
+				heap[root] = heap[c];
+				heap[c] = temp; 
+			}
+			c = root; 
+		}while (c != 0);
+	}
+	//크기를 줄여가며 반복적으로 힙을 구성 
+	for (int i = number - 1; i >= 0; i--) {
+		int temp = heap[0];
+		heap[0] = heap[i];
+		heap[i] = temp; 
+		int root = 0; 
+		int c = 1; 
+
+		do {
+			c = 2 * root + 1; 
+			//자식 중에 더 큰값을 찾기 
+			if (c < i - 1 && heap[c] < heap[c + 1]) {
+				c++;
+			}
+			//루트보다 자식이 크다면 교환 
+			if (c < i && heap[root] < heap[c]) {
+				temp = heap[0];
+				heap[0] = heap[i];
+				heap[i] = temp;
+			}
+			root = c; 
+		} while (c < i);
+	}
+	for (int i = 0; i < number; i++) {
+		printf("%d ", heap[i]);
+	}
+	return 0; 
+}
