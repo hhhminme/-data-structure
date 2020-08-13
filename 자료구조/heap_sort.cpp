3462cedb8eb23f6c4f8e51caad1d43ada09dc49e
hhhminme <kenma409@gmail.com>
2020-08-13 21:46:56 +0900

    heapsort review

--- 자료구조/heap_sort.cpp
+++ 자료구조/heap_sort.cpp
@@ -1,46 +1,46 @@
 #include <stdio.h>
+#define HEAPSIZE 9
 
-int number = 9; 
-int heap[9] = { 7,6,5,8,3,5,9,1,6 };
+int heap[HEAPSIZE] = { 1,5,3,6,2,6,6,4,9 };
+
+//힙 구조 만들기 
 
 int main(void) {
-	//힙을 구성 
-	for (int i = 1; i < number; i++) {
+	for (int i = 1; i < HEAPSIZE; i++) {
 		int c = i; 
 		do {
 			int root = (c - 1) / 2;
 			if (heap[root] < heap[c]) {
 				int temp = heap[root];
 				heap[root] = heap[c];
-				heap[c] = heap[root];
-			}
-			c = root;
+				heap[c] = temp;
+			} c = root; 
 		} while (c != 0);
 	}
-	//크기를 줄여가며 반복적으로 힙을 구성
-	for (int i = number - 1; i >= 0; i--) {
-		int temp = heap[0];
-		heap[0] = heap[i];
-		heap[i] = temp;
-		int root = 0;
-		int c = 1;
+	//크기를 줄여가며 힙 정렬
+	for (int i = HEAPSIZE - 1; i >= 0; i--) {
+		int temp = heap[0]; 
+		heap[0] = heap[i]; 
+		heap[i] = temp; 
+		int root = 0; 
+		int c = 1; 
+		//자식 중에 큰 값이 있는지 확인 
 		do {
-			c = 2 * root + 1;
-			//자식 중에 더 큰 값을 찾기 
+			c = 2 * root + 1; 
 			if (c < i - 1 && heap[c] < heap[c + 1]) {
 				c++;
 			}
+		//root 값이 더 크다면 교체 
 			if (c < i && heap[root] < heap[c]) {
-				//루트보다 자식이 크다면 교환 
-				temp = heap[root];
-				heap[root] = heap[c];
+				temp = heap[c];
 				heap[c] = heap[root];
+				heap[root] = temp; 
 			}
-			root = c;
+			root = c; 
 		} while (c < i);
 	}
-	for (int i = 0; i < number; i++) {
-		printf("%d", heap[i]);
+	for (int i = 0; i < HEAPSIZE; i++) {
+		printf("%d ", heap[i]);
 	}
-	return 0; 
+	return 0;
 }
--- 자료구조/test.cpp
+++ 자료구조/test.cpp
@@ -1,47 +1,64 @@
-#include <stdio.h>
+//하향식 heapsort
 
-int number = 9; 
-int heap[9] = { 7,6,5,8,3,5,9,1,6 };
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int number; 
+int heap[1000001];
+
+void heapify(int i) {
+	//왼쪽 자식 노드를 가리킵니다. 
+	int c = 2 * i + 1; 
+	//오른쪽 자식 노드가 있고, 왼쪽 자식노드보다 크다면
+	if (c < number && heap[c] < heap[c + 1]) {
+		c++;
+	}
+	//부모보다 자식이 더 크다면 위치를 교환 
+	if (heap[i] < heap[c]) {
+		int temp = heap[i];
+		heap[i] = heap[c];
+		heap[c] = temp;
+	}
+	//number / 2 까지만 수행하면 된다. 
+	if (c <= number / 2) {
+		heapify(c);
+	}
+}
 
 int main(void) {
-	// 힙 구성 
-	for (int i = 1; i < number; i++) {
-		int c = i; 
-		do {
-			int root = (c - 1) / 2; 
-			if (heap[root] < heap[c]) {
-				int temp = heap[root];
-				heap[root] = heap[c];
-				heap[c] = temp; 
-			}
-			c = root; 
-		}while (c != 0);
+	cin >> number;
+	for (int i = 0; i < number; i++) {
+		int x; 
+		cin >> heap[i];
 	}
-	//크기를 줄여가며 반복적으로 힙을 구성 
-	for (int i = number - 1; i >= 0; i--) {
+	//힙 생성
+	for (int i = number / 2; i >= 0; i--) {
+		heapify(i);
+	}
+	//힙 정렬 
+	for (int i = number - 1; i>= 0; i--) {
+		for (int j = 0; j < number; j++) {
+			cout << heap[j] << ' ';
+		}
+		cout << '\n';
 		int temp = heap[0];
 		heap[0] = heap[i];
-		heap[i] = temp; 
+		heap[i] = temp;
 		int root = 0; 
 		int c = 1; 
-
 		do {
 			c = 2 * root + 1; 
-			//자식 중에 더 큰값을 찾기 
 			if (c < i - 1 && heap[c] < heap[c + 1]) {
 				c++;
 			}
-			//루트보다 자식이 크다면 교환 
 			if (c < i && heap[root] < heap[c]) {
-				temp = heap[0];
-				heap[0] = heap[i];
-				heap[i] = temp;
+				temp = heap[root];
+				heap[root] = heap[c];
+				heap[c] = temp;
 			}
 			root = c; 
 		} while (c < i);
 	}
-	for (int i = 0; i < number; i++) {
-		printf("%d ", heap[i]);
-	}
-	return 0; 
 }
